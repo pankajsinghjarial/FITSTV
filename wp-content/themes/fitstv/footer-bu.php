a6bbd364ff106d476df782410a5b8ee42cca0030
Crazy <root@thakur.(none)>
2015-10-23 07:59:19 +0530

    init commit

---
+++ wp-content/themes/fitstv/footer-bu.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+
+
+<div class="footer_top">
+	<div class="inner-w">
+    <div class="news_letter">
+      <label>Subscribe to Our Monthly Newsletter</label>
+        <div class="input-group">
+            <span><img src="<?php echo get_template_directory_uri();?>/images/masseges.jpg" width="29" height="24" alt="messages"></span>
+           <input type="text" id="subscribe-email" class="form-control input-lg" placeholder="Enter your email adress here">
+           <button id="btn-subscribe" type="button" class="btn btn-primary">SUBSCRIBE</button>
+            <?php //echo do_shortcode('[contact-form-7 id="89" title="Subscribe Form Footer"]');?>
+      </div>
+    </div>
+    </div>
+</div>
+
+<footer>
+    <div class="footer_bg">
+    <div class="inner-w">
+    	
+        <div class="socialAreaSlider full-w">
+        	<h3><img src="<?php echo get_template_directory_uri();?>/images/s-twitter.png" width="30" height="21" alt="Twitter"> Tweets</h3>
+        	<ul class="socialAreaSliderIn">
+            	<li>    
+                <figcaption>
+                <figure><img src="<?php echo get_template_directory_uri();?>/images/social01.jpg" width="90" height="90" alt="social"></figure>
+                <span>
+                <strong>Fitse TV</strong><br>
+				@fitsetv
+                </span>
+                </figcaption>
+                <div class="clear"></div>
+				<em>16 Nov</em>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh  dolor sit amet, consectetuer adipiscing elit, 
+                	<a href="javascript:void(0);">#euismod</a></p>
+                </li>
+                <li>
+                <figcaption>
+                <figure><img src="<?php echo get_template_directory_uri();?>/images/social01.jpg" width="90" height="90" alt="social"></figure>
+                <span>
+                <strong>Fitse TV</strong><br>
+				@fitsetv
+                </span>
+                </figcaption>
+                <div class="clear"></div>
+				<em>16 Nov</em>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh  dolor sit amet, consectetuer adipiscing elit, 
+                	<a href="javascript:void(0);">#euismod</a></p>
+                </li>
+                <li>
+                <figcaption>
+                <figure><img src="<?php echo get_template_directory_uri();?>/images/social01.jpg" width="90" height="90" alt="social"></figure>
+                <span>
+                <strong>Fitse TV</strong><br>
+				@fitsetv
+                </span>
+                </figcaption>
+                <div class="clear"></div>
+				<em>16 Nov</em>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh  dolor sit amet, consectetuer adipiscing elit, 
+                	<a href="javascript:void(0);">#euismod</a></p>
+                </li>
+            </ul>
+            <div class="clear"></div>
+            <figure class="sTweets"><img src="<?php echo get_template_directory_uri();?>/images/tweets-img.png" width="298" height="20" alt="tweets"></figure>
+        </div>
+        <?php if ( is_active_sidebar( 'footer-twitter-slides' ) ) {
+				//dynamic_sidebar( 'footer-twitter-slides' ); 
+			  }
+		?>
+        <div class="footer_menu">
+            
+            <?php wp_nav_menu( array( 'container' => '', 'theme_location' => 'primary', 'menu_class' => 'nav-menu', 'menu_id' => 'primary-menu' ,'before' => '','after' => '','link_before' => '','link_after' => '','items_wrap' => '%3$s','depth' => 0,	'walker' => '') ); ?>
+        </div>
+        <div class="footer_bootam">
+            <ul>
+                <li><span class="fo1">Copyright © 2015</span><span class="fo2"> All Rights Reserved<i class="lll"></i> Fitse.tv</span> </li>
+                <li><a href="https://www.facebook.com/FitseTV#"><img src="<?php echo get_template_directory_uri();?>/images/fb.png" alt="FB"></a></li>
+                <li><a href="https://www.pinterest.com/FitseTV/"><img src="<?php echo get_template_directory_uri();?>/images/pin_icon.png" alt=""/></a></li>
+                <li><a href="javascript:void(0);"><img src="<?php echo get_template_directory_uri();?>/images/in_icon.png" alt=""/></a></li>
+                <li><a href="https://instagram.com/fitsetv/"><img src="<?php echo get_template_directory_uri();?>/images/in2_icom.png" alt=""/></a></li>
+                <li><a href="https://twitter.com/fitsetv"><img src="<?php echo get_template_directory_uri();?>/images/twitter.png" alt=""/></a></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+    
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/fitstv/js/function (copy).js
@@ -0,0 +1,205 @@
+
+$(document).ready(function(){
+
+/*pop-up*/
+	$('.my_modal_open').click(function(){
+		$('.my_modal').fadeIn('slow');
+	});
+	$('.my_modal_close').click(function(){
+		$('.my_modal').fadeOut('slow');	
+	});
+	
+	$(".menu").click(function(){
+		$("nav").slideToggle();	
+	});
+	
+	$(".sideNav h4").click(function(){
+		$(".sideNav ul").slideToggle();	
+	});
+	
+/*Ask a Question*/
+	$('.askAQtn').click(function(){
+		$('.askAQtnPop, .overlayBg').fadeIn('slow');
+	});
+	$('.my_modal_close').click(function(){
+		$('.askAQtnPop, .overlayBg').fadeOut('slow');	
+	});
+	
+	
+	var offset = $("#sidebar").offset();
+	var topPadding = 15;
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > offset.top) {
+			$("#sidebar").stop().animate({
+				marginTop: $(window).scrollTop() - offset.top + topPadding
+			});
+		} else {
+			$("#sidebar").stop().animate({
+				marginTop: 0
+			});
+		};
+	});
+	
+/******************************
+ portSlideIn*******************/
+  if($(".bxsliderbasic").length){	
+  $('.bxsliderbasic').bxSlider({
+	 infiniteLoop: false,
+	 hideControlOnEnd: true,
+	 auto: true,
+	 speed: 1000
+  });
+  }
+/******************************
+ basicSliders*******************/	
+ if($(".basicSliders").length){	
+  $('.basicSliders').bxSlider({
+	 minSlides: 1,
+	 maxSlides: 4,
+	 slideWidth: 201,
+	 slideMargin: 10,
+	 moveSlides:1,
+	 pager:false
+  });  
+ }
+/******************************
+ basicSlidersAuto*******************/
+  if($(".basicSlidersAuto").length){	
+  $('.basicSlidersAuto').bxSlider({
+	 minSlides: 1,
+	 maxSlides: 4,
+	 slideWidth: 201,
+	 slideMargin: 10,
+	 moveSlides:1,
+	 pager:false,
+	 auto: false,
+	 speed: 1000,
+	 pause: 1000
+  });
+  }
+  
+/******************************
+ basicSliders2*******************/	
+ if($(".basicSliders2").length){
+  $('.basicSliders2').bxSlider({
+	 minSlides: 1,
+	 maxSlides: 5,
+	 slideWidth: 201,
+	 slideMargin: 20,
+	 moveSlides:1,
+	 pager:false
+  });
+ }
+ 
+/******************************
+ bxsliderTv*******************/	
+  if($(".bxsliderTv").length){
+ $('.bxsliderTv').bxSlider({
+	 infiniteLoop: false,
+	 hideControlOnEnd: true,
+	 pager:false
+  }); 
+  }
+ 
+/******************************
+ bxsliderTv*******************/
+if($(".thumbSlider").length){  
+$('.thumbSlider').bxSlider({
+  pagerCustom: '#bx-pager',
+  captions: true
+});
+}
+
+
+/******************************
+ portSlideIn*******************/  
+  var owl = $("#owl-demo");
+
+  owl.owlCarousel({ 
+
+  items : 5, //10 items above 1000px browser width
+  itemsDesktop : [1000,5], //5 items between 1000px and 901px
+  itemsDesktopSmall : [900,3], // 3 items betweem 900px and 601px
+  itemsTablet: [600,2], //2 items between 600 and 0;
+  itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+  
+  });
+
+  // Custom Navigation Events
+  /*$(".next").click(function(){
+	owl.trigger('owl.next');
+  })
+  $(".prev").click(function(){
+	owl.trigger('owl.prev');
+  })
+  $(".play").click(function(){
+	owl.trigger('owl.play',1000);
+  })
+  $(".stop").click(function(){
+	owl.trigger('owl.stop');
+  })*/
+
+
+/******************************
+ Episodes Tab*******************/
+ if($(".sortTabber").length){ 	
+   $( ".sortTabber" ).tabs();	
+ }
+ 
+
+ 
+ 
+});
+
+
+$(document).ready(function(){
+	var submitIcon = $('.searchbox-icon');
+	var inputBox = $('.searchbox-input');
+	var searchBox = $('.searchbox');
+	var isOpen = false;
+	submitIcon.click(function(){
+		if(isOpen == false){
+			searchBox.addClass('searchbox-open');
+			inputBox.focus();
+			isOpen = true;
+		} else {
+			searchBox.removeClass('searchbox-open');
+			inputBox.focusout();
+			isOpen = false;
+		}
+	});  
+	 submitIcon.mouseup(function(){
+			return false;
+		});
+	searchBox.mouseup(function(){
+			return false;
+		});
+	$(document).mouseup(function(){
+			if(isOpen == true){
+				$('.searchbox-icon').css('display','block');
+				submitIcon.click();
+			}
+		});
+});
+function buttonUp(){
+		var inputVal = $('.searchbox-input').val();
+		inputVal = $.trim(inputVal).length;
+		if( inputVal !== 0){
+			$('.searchbox-icon').css('display','none');
+		} else {
+			$('.searchbox-input').val('');
+			$('.searchbox-icon').css('display','block');
+		}
+}
+
+/******************new for footer******************/
+ /******************************
+ socialAreaSlider*******************/
+  if($(".socialAreaSliderIn").length){	
+  $('.socialAreaSliderIn').bxSlider({
+	 infiniteLoop: true,
+	 hideControlOnEnd: true,
+	 auto: true,
+	 speed: 1000
+  });
+  }
